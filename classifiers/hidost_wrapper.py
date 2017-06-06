4b82ee6128534805acb4847277dd72fc173b7f2d
Helen Simecek <hes5tp@virginia.edu>
2017-06-06 16:06:24 -0400

    Fix out-of-order classifier scores in hidost_wrapper.py
    Corrects the file order of input PDFs to classifier output scores, so that scores are returned in the same order the PDFs were input (previously not guaranteed due to libquickly).

--- classifiers/hidost_wrapper.py
+++ classifiers/hidost_wrapper.py
@@ -46,6 +46,22 @@ def load_features():
 
 feats = load_features()
 
+def correct_feats_order(feats_fname, flist_fname, feats_fname_ordered):
+    with open(flist_fname) as f:
+        flist = [ line.strip() for line in f.readlines() ]
+
+    with open(feats_fname) as f:
+        feat_fname_list = [ line.strip().split('#') for line in f.readlines() ]
+
+    feat_list, fname_list = zip(*feat_fname_list)
+    fname_feats = dict(zip(fname_list, feat_list))
+    # print flist
+    feat_list_sorted = [ fname_feats[fname] for fname in flist ]
+
+    with open(feats_fname_ordered, 'w') as f:
+        for feat, fname in zip(feat_list_sorted, flist):
+            f.write("%s#%s\n" % (feat, fname))
+
 def hidost_feature(pdf_paths):
     if not os.path.isdir(cache_dir):
         os.system("mkdir -p %s" % (cache_dir))
@@ -63,33 +79,39 @@ def hidost_feature(pdf_paths):
     pdf_file_list = os.path.join(tmp_dir, "input.pdfs.list")
     
     data_file = os.path.join(tmp_dir, "input.data.libsvm")
+    data_file_corrected_order = os.path.join(tmp_dir, "input.data.ordered.libsvm")
     
-    cmd = "echo '%s' > %s " % ("", pdf_file_list)
-    os.system(cmd)
+    # cmd = "echo '%s' > %s " % ("", pdf_file_list)
+    # os.system(cmd)
 
     # ---pdf-file-----> pdf2paths ---->  pdf-path-file
     for pdf_path in pdf_paths:
         #pdf_file_name = os.path.basename(pdf_path)
         pdf_file_name = hash_str(pdf_path) + ".pdf"
 
-        cmd = "%s \"%s\" y > %s" % (pdf2paths_cmd, pdf_path, os.path.join(tmp_dir, pdf_file_name))
+        cmd = "%s \"%s\" n > %s" % (pdf2paths_cmd, pdf_path, os.path.join(tmp_dir, pdf_file_name))
         #print cmd
         os.system(cmd)
-        cmd = "echo '%s\n' >> %s " % (os.path.join(tmp_dir, pdf_file_name), pdf_file_list)
+        cmd = "echo '%s' >> %s " % (os.path.join(tmp_dir, pdf_file_name), pdf_file_list)
         os.system(cmd)
 
     # ----pdf-path-files----> feat-extract ------>  data.libsvm
     cmd = "%s -m %s -b %s -f %s -o %s" % (feat_extract_cmd, pdf_file_list, empty_file_list, feats_path, data_file)
+    print(cmd)
     os.system(cmd)
+    os.system("cat %s" % data_file)
+
+    # Critical: the file order in input.pdfs.list may not be preserved in input.data.libsvm due to libquickly.
+    correct_feats_order(data_file, pdf_file_list, data_file_corrected_order)
 
     #print 'Loading data file'
-    X, _ = datasets.load_svmlight_file(data_file, 
+    X, _ = datasets.load_svmlight_file(data_file_corrected_order, 
                                        n_features=6087, 
                                        multilabel=False, 
                                        zero_based=False, 
                                        query_id=False)
     X = X.toarray()
-    os.system("rm -rf %s" % tmp_dir)
+    # os.system("rm -rf %s" % tmp_dir)
     return X
 
 def get_feature_names(x):
